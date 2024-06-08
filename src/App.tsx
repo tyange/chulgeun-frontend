e150805a608284e969ea4f6091666e512965668c
tyange <usun16@gmail.com>
2024-06-08 09:39:33 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-red-50">chulgeun</p>
+        <button className="btn">Snoooze</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
