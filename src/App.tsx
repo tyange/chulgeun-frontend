3bb35bbcd550b866a39de891e201d20ffd5cef2e
tyange <usun16@gmail.com>
2024-06-09 18:37:40 +0900

    work start test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { formatISO } from "date-fns";
+
 function App() {
+  const workStartHandler = async () => {
+    try {
+      const res = await fetch("http://localhost:8080/work/start", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          start_at: formatISO(new Date()),
+          company_name: "kfc",
+        }),
+      });
+      console.log(await res.json());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <div>
         <p className="text-red-50">chulgeun</p>
-        <button className="btn">Snoooze</button>
+        <button className="btn" onClick={workStartHandler}>
+          Work Start
+        </button>
       </div>
     </>
   );
