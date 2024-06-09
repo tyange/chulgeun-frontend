b1ad3392a854816516dd61f3c36d6ecbd743a26b
tyange <usun16@gmail.com>
2024-06-09 20:27:19 +0900

    timer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
-import { formatISO } from "date-fns";
+import { useState } from "react";
+import { useInterval } from "usehooks-ts";
+import { formatISO, addSeconds } from "date-fns";
+
+const INTERVAL_DELAY = 1000;
 
 function App() {
+  const [time, setTime] = useState<Date | null>(null);
+  const [isPause, setIsPause] = useState<number | null>(null);
+
+  useInterval(() => {
+    setTime((prevState) => {
+      if (!prevState) {
+        return null;
+      }
+
+      return addSeconds(prevState, -1);
+    });
+  }, isPause);
+
+  const pauseHandler = () => {
+    setIsPause(null);
+  };
+
   const workStartHandler = async () => {
     try {
       const res = await fetch("http://localhost:8080/work/start", {
@@ -11,7 +32,11 @@ function App() {
           company_name: "kfc",
         }),
       });
-      console.log(await res.json());
+
+      const data = await res.json();
+
+      setTime(new Date(data.work["start_at"]));
+      setIsPause(INTERVAL_DELAY);
     } catch (err) {
       console.log(err);
     }
@@ -20,10 +45,18 @@ function App() {
   return (
     <>
       <div>
-        <p className="text-red-50">chulgeun</p>
-        <button className="btn" onClick={workStartHandler}>
-          Work Start
-        </button>
+        <p className="text-red-50">
+          {time ? time.toString() : "not start work"}
+        </p>
+        {isPause !== null ? (
+          <button className="btn btn-warning" onClick={pauseHandler}>
+            Pause
+          </button>
+        ) : (
+          <button className="btn btn-primary" onClick={workStartHandler}>
+            Work Start
+          </button>
+        )}
       </div>
     </>
   );
