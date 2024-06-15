b3f865e5fdd840f699bd171266e63069261c8386
tyange <usun16@gmail.com>
2024-06-15 16:24:25 +0900

    Box 컴포넌트 스타일링 수정

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -7,7 +7,7 @@ type BoxProps = {
 
 export default function Box({ comingSoon = false, children }: BoxProps) {
   return (
-    <div className="flex w-fit items-center justify-center rounded-lg bg-white bg-opacity-90 p-10 text-black">
+    <div className="flex w-fit items-center justify-center rounded-lg border border-gray-300 bg-white bg-opacity-90 p-10 text-black shadow-md">
       {comingSoon ? "Coming Soon!" : children}
     </div>
   );
