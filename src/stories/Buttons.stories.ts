aae2eb44204c52f1b1c638024de48814719afa74
tyange <usun16@gmail.com>
2024-06-15 15:37:13 +0900

    코드 정리

--- src/stories/Buttons.stories.ts
+++ src/stories/Buttons.stories.ts
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
+
 import Button from "@/components/ui/Button";
+
 import { Sizes } from "@/constants/Sizes";
 import { Colors } from "@/constants/Colors";
 
