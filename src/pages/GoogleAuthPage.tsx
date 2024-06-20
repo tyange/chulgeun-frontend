65ef6f2d28a046ce04ae60eb6af2429bfa198f05
tyange <usun16@gmail.com>
2024-06-20 21:19:10 +0900

    end commit

--- src/pages/GoogleAuthPage.tsx
+++ src/pages/GoogleAuthPage.tsx
@@ -1,33 +1,44 @@
+import { useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useMutationState } from "@tanstack/react-query";
 import { match } from "ts-pattern";
 import axios from "axios";
 
 import Box from "@/components/ui/Box";
-import { useEffect } from "react";
 
 export default function GoogleAuthPage() {
+  const initialized = useRef(false);
+
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const code = searchParams.get("code");
 
   const mutation = useMutation({
-    mutationFn: (c: string) =>
-      axios.post("http://localhost:8080/auth/google", { code: c }),
+    mutationKey: ["googleLogin"],
+    mutationFn: async (c: string) =>
+      await axios.post("http://localhost:8080/auth/google", { code: c }),
     // TODO: 성공일 때의 처리
+    onSuccess: () => console.log("hi"),
     // TODO: 에러 발생했을 때의 처리 (code가 만료됐을 경우, 실제 네트워크 오류인 경우를 분리)
+    onError: () => console.log("bye"),
+  });
+
+  const [mutationState] = useMutationState({
+    filters: { mutationKey: ["googleLogin"] },
+    select: (mutation) => mutation.state,
   });
 
   useEffect(() => {
-    if (code) {
+    if (code && !initialized.current) {
       mutation.mutate(code);
+      initialized.current = true;
     }
   }, []);
 
   return (
     <Box>
       <p>
-        {match(mutation)
+        {match(mutationState)
           .with({ status: "pending" }, () => "구글 로그인을 시도하는 중입니다.")
           .with({ status: "error" }, () => "구글 로그인에 실패했습니다.")
           .with(
